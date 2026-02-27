1a267e00fc871df1793626695bde571d1ee4632e
AtesiT <atesit@mail.ru>
2026-02-27 23:28:57 +0300

    ADD: 2 buttons (one have modifiers), 2 colorSet (buttonBorder, buttonText)

--- MoviesSwiftUI/HomeView.swift
+++ MoviesSwiftUI/HomeView.swift
@@ -15,6 +15,29 @@ struct HomeView: View {
                 //  Пока прогружается фото, отображается "ромашка"
                 ProgressView()
             }
+            
+            HStack {
+                //  Добавляем одну кнопку
+                Button {
+                    //  TODO: - Здесь функционал кнопки
+                } label: {
+                    Text(Constants.playString)
+                        .frame(width: 100, height: 50)
+                        .foregroundStyle(.buttonText)
+                        .bold()
+                        .background {
+                            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                .stroke(.buttonBorder, lineWidth: 5)
+                        }
+                }
+                //  Добавляем вторую кнопку
+                Button {
+                    
+                } label: {
+                    Text(Constants.downloadString)
+                }
+
+            }
         }
     }
 }
